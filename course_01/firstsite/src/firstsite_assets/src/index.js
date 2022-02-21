33210f48acf8c7f1c9bca084226d543d575e1d76
ququzone <xueping.yang@gmail.com>
2022-02-21 10:19:22 +0800

    initial project

---
+++ course_01/firstsite/src/firstsite_assets/src/index.js
@@ -0,0 +1,19 @@
+import { firstsite } from "../../declarations/firstsite";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await firstsite.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
